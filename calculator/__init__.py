a4dae7f9594b06ade635cddeaf8df15e1cefeb74
ShriyaSharma1122334455 <shriyasharma2152@gmail.com>
2025-02-04 15:13:13 -0500

    Upgraded pylintrc file with test cases

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -1,10 +1,10 @@
 """This module provides basic calculator test."""
 
-def add(a, b):
+def add(num1, num2):
     """Returns the sum of a and b."""
-    return a + b
-def substract(a,b):
+    return num1 + num2
+def substract(num1,num2):
     """Returns the difference of a and b."""
-    return a-b
+    return num1-num2
 
 # Ensure the file ends with a newline
