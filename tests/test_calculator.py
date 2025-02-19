221eae0775c168ac8e0df29f069692c620755197
ShriyaSharma1122334455 <shriyasharma2152@gmail.com>
2025-02-18 22:13:32 -0500

    Added Main.py to display the output and a Test File for the same

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -3,7 +3,7 @@ from calculator import operations
 
 def test_addition():
     '''Test that addition function works '''    
-    assert operations.add(2,2) == 4
+    assert operations.add(2,9) == 11
 
 def test_subtraction():
     '''Test that addition function works '''    
@@ -11,8 +11,8 @@ def test_subtraction():
 
 def test_multiply():
     '''Test that addition function works '''    
-    assert operations.multiply(2,2) == 4
+    assert operations.multiply(2,6) == 12
 
 def test_divide():
     '''Test that addition function works '''    
-    assert operations.divide(2,2) == 1
+    assert operations.divide(6,6) == 1
